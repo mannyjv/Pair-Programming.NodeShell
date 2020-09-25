46d1425d6da162c955f2b2b3c51afe4b95127629
Peter Boustani <boustanip718@gmail.com>
2020-09-25 15:08:43 -0400

    up to 4.Bonus: Command Arguments

--- bash.js
+++ bash.js
@@ -1,11 +1,18 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 process.stdout.write("prompt > ");
 
 process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === "pwd") {
-    process.stdout.write(process.cwd());
-  }
-
-  process.stdout.write("You typed: " + cmd);
-  process.stdout.write("\nprompt > ");
+    const cmd = data.toString().trim();
+    if (cmd === "pwd") {
+        pwd();
+    }
+    else if (cmd === 'ls') {
+        ls();
+    }
+    else {
+      process.stdout.write("You typed: " + cmd);
+      process.stdout.write("\nprompt > ");
+    }
 });
